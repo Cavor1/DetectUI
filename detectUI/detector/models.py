45ee67785663ce15fb465ada2e00e18646b8786a
Cavor1 <cavor1111@gmail.com>
2025-08-04 12:09:29 +0200

    clean

--- detectUI/detector/models.py
+++ detectUI/detector/models.py
@@ -1,6 +1,7 @@
+from typing import Mapping
 from django.db import models
 from django.conf import settings
-
+from django.utils import timezone
 def image_path(instance,filename):
     return f'uploads/user_{instance.user.id}/{filename}'
 
@@ -13,6 +14,28 @@ class UploadedImage(models.Model):
     image = models.ImageField(upload_to=image_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
-class AiModel(models.Model):
-    name = models.CharField()
-    
+class Conversation(models.Model):
+    image = models.ForeignKey(
+        UploadedImage,
+        on_delete=models.CASCADE,
+        related_name='conversations'
+    )
+    started_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["-modified_at"]
+
+
+class Message(models.Model):
+    ROLE_CHOICES = {
+        'user':'user',
+        'assistant':'assistant'
+    }
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    role= models.CharField(max_length=10, choices=ROLE_CHOICES)
+
