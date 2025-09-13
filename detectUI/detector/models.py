553ee81990519020c008c691779207206d23da79
Cavor1 <cavor1111@gmail.com>
2025-09-13 14:43:12 +0200

    detection model, detections stored and retrieved from model

--- detectUI/detector/models.py
+++ detectUI/detector/models.py
@@ -2,6 +2,7 @@ from typing import Mapping
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+
 def image_path(instance,filename):
     return f'uploads/user_{instance.user.id}/{filename}'
 
@@ -13,3 +14,22 @@ class UploadedImage(models.Model):
     ) 
     image = models.ImageField(upload_to=image_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Detection(models.Model):
+    inference_model = models.CharField(max_length=64,db_index=True)
+    image = models.ForeignKey(
+        UploadedImage,
+        on_delete=models.CASCADE,
+        related_name="detections",
+        db_index=True
+    )
+    detections = models.JSONField(null = True,blank = True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["image", "inference_model"],
+                name="uniq_detection_per_image_model",
+            )
+        ]
+
--- detectUI/detector/views.py
+++ detectUI/detector/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from .models import UploadedImage
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import UploadedImage, Detection
 from django.contrib.auth.decorators import login_required
 from .forms import ImageUploadForm
 from .services.registry import inference_models
 from PIL import Image
+
+
+def addDetection(image : UploadedImage,inference_model : str , labels):
+    Detection.objects.update_or_create(
+        image = image,
+        inference_model = inference_model,
+        defaults={
+            "detections" : labels,
+        }
+    )
+
 
 
 @login_required
@@ -20,9 +31,13 @@ def main(request):
         .filter(user=request.user)
     )
     selected_id = request.GET.get('img')
+    selected_model = request.GET.get('model')
     selected_img = UploadedImage.objects.filter(pk=selected_id,user=request.user).first()
-
-
+    detection = Detection.objects.filter(image = selected_img, inference_model = selected_model).first()
+    if detection is not None:
+        detections = detection.detections
+    else:
+        detections = None
     if request.method == 'POST':
 
         form_name = request.POST.get('form_name')
@@ -33,17 +48,21 @@ def main(request):
                 img.user = request.user
                 img.save()
                 return redirect('main')
+
+
         if form_name == 'form_detect' and selected_img is not None:
             model = request.POST.get('model')
             try:
                 img = Image.open(selected_img.image.path)
-                result = inference_models[model].detect(img)  # your InferenceResult
+                result = inference_models[model].detect(img)
                 labels = result.labels
-                labels = labels
+                addDetection(selected_img,model,labels)
+
             except Exception as e:
                 labels = [f'{e}']
 
 
+            return HttpResponseRedirect(f'/?img={selected_id}&model={selected_model}')
 
 
 
@@ -51,7 +70,8 @@ def main(request):
         'images':images,
         'form':form,
         'selected':selected_img,
-        'model_list':inference_models,
-        'labels': labels
+        'selected_model':selected_model,
+        'model_list':list(inference_models.keys()),
+        'labels': detections
     })
 # Create your views here.
